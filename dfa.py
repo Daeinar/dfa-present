0505f42064a72c14e64094ad49ef3f47d836258f
Philipp Jovanovic <p.jo@gmx.net>
2013-05-01 22:03:34 +0200

    comments

--- dfa.py
+++ dfa.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 from present import Present
 
+#
+# Simulates fault injection on the PRESENT key schedule
+#
+
 if __name__ == '__main__':
   # 80-bit fault injection
   p80 = Present(ks=80)
   m = 0x0000000000000000
   k = 0x00000000000000000000
-  f = (31,0x00000000000000000000) # (fault round, fault value)
+  f = (31,0x00000000000000000000) # (fault round, 80-bit fault value)
   c  = p80(m,k)
   cf = p80(m,k,f)
   print "c      = {:016x}".format(c)
